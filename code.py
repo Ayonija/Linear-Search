7c6a107082dd4197aa55bde747f208ae2713b4a4
Ayonija Mathur <44442832+Ayonija@users.noreply.github.com>
2019-07-13 14:25:56 +0530

    Update code.py
    Brute Force Algorithms refers to a programming style that does not include any shortcuts to improve performance this is properly explained using the above python code with proper comments in the code.

--- code.py
+++ code.py
@@ -1,12 +1,18 @@
 # Implementation of  Linear Search using Brute Force Approach in Python
 # date created :13 July 2019
+
+""" Taking Input """
 n=int(input("Enter the number of items :"))
 items=[]
 while n>0 :
   m=int(input("Enter items in the list :"))
   items= items+ [m]
   n=n-1
+  
+""" Printing the inputed Items """
 print("list of items are :",items)
+
+""" Main Linear Search code using Brute Force Approach """
 x=int(input("Enter item to be searched :")) 
 i=flag=0
 while i<len(items):
@@ -14,7 +20,10 @@ while i<len(items):
     flag=1
     break
   i=i+1
+
+""" Making the user update about the output of the search """
 if flag==1:
   print("Item found at Position :",i+1)
 else:
   print("Item not Found")
+  
